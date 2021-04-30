aeae7c1c6ef82c0c88cdf4f16e62943b3bbb9b70
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-30 17:22:10 +0200

    Correzione dati

--- index.php
+++ index.php
@@ -38,23 +38,28 @@
       ],
     ],
     [
-      'title' => 'Perché il mio account è associato a un paese?',
+      'title' => "Perché il mio account è associato a un paese?",
       'info' => [
         [
-          "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:",
+          "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose: <br><br>
+            <ol>
+              <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti: <br><br>
+                <ul>
+                  <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li> <br>
+                  <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li> <br>
+                </ul>
+              </li>
+              <li>
+                La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+              </li>
+            </ol> <br><br>
+          Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
         ],
-        [
-          "La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-          Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
-          Google LLC, con sede negli Stati Uniti, per il resto del mondo.
-          La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali."
-        ],
-        [
-          "Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.",
-        ],
-      ],
-      'subtitle' => "Stabilire il paese associato al tuo account",
-      'description' => [
+      ]
+    ],
+    [
+      'title' => "Stabilire il paese associato al tuo account",
+      'info' => [
         [
           "Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.",
         ],
@@ -64,7 +69,7 @@
         [
           "Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.",
         ],
-      ],
+      ]
     ],
     [
       'title' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
@@ -122,9 +127,15 @@
 <main>
 <?php
   for ($i=0; $i < count($testi) ; $i++) {
-    var_dump($testi[$i]);
-  }
- ?>
+    ?>
+    <div class="">
+      <?=  $testi[$i]['title'] ?> <br>
+      <?=  $testi[$i]['info'] ?>
+    </div>
+
+
+  <?php } ?>
+
 </main>
 
 <footer>
